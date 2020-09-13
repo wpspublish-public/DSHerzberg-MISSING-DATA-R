c96669806c523987a02726fc953a75af488d28e5
David Herzberg <dherzberg@wpspublish.com>
2020-09-13 10:45:53 -0700

    test new BLIMP script from Enders

--- CODE/Blimp-Scratch.R
+++ CODE/Blimp-Scratch.R
@@ -1,15 +1,31 @@
 suppressMessages(library(here))
 suppressMessages(library(tidyverse))
 
-outboard_1 <- read_csv(here(
-  "OUTPUT-DEDICATED-BLIMP-PROCESSOR/GOAL2-impute1.csv"
-))
+input_orig <- suppressMessages(read_csv(here(
+  "ENDERS-REDUCE-PROC-TIME/TOD.DATA.3.5.20_forBLIMP7.14.20.csv"
+))) 
 
-MBP_1 <- read_csv(here(
-  "GOAL-EXAMPLE/GOAL-impute1.csv"
-))
-identical(outboard_1, MBP_1)
-all.equal(outboard_1, MBP_1)
+NA_count <- sum(is.na(input_orig))
+NA_count
 
-sum(outboard_1 == 999)
-sum(MBP_1 == 999)
+
+impute_name <- "TOD-impute1"
+impute_path <- "ENDERS-REDUCE-PROC-TIME/"
+
+temp1 <- suppressMessages(
+  read_csv(
+    here(
+      str_c(impute_path, impute_name, ".csv")
+    ),
+    col_names = F))
+names(temp1) <- c("id", "item", "response")
+temp2 <- temp1 %>% 
+  pivot_wider(
+    id_cols = id,
+    names_from = item,
+    values_from = response
+  )
+names(temp2) <- names(input_orig)
+
+NA_count <- sum(temp2 == 999)
+NA_count
